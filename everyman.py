eea9d559456d58d198117ae6b9f170bf4cb527ee
Tom Pakeman <tegpakeman@gmail.com>
2019-05-13 12:11:17 +0100

    Removed test code from everyman.py

--- everyman.py
+++ everyman.py
@@ -63,22 +63,3 @@ def scrape_everyman(venues: list, timeconfig: dict=None):
                 html += templates['end_times']
                 plaintext += f"\n"
     return html, data, plaintext
-
-
-if __name__ == '__main__':
-    CONFIG = 'config.json'
-    with open(CONFIG, 'r', encoding='UTF-8') as f:
-        config = json.loads(f.read())
-    venues = config['venues']
-    recipients = config['recipients']
-    creds = config['email']
-    times = config['times']
-    html, data, text = scrape_everyman(venues, times)
-
-    send_html_mail(text,
-                   html,
-                   'Cinema times this week',
-                   recipients,
-                   'Python Mail',
-                   'gmail',
-                   creds)
